483758e26ae19a2cb7df63948362704dc687bf49
Harvey Sanders <sanders.harvey@gmail.com>
2022-03-14 13:20:09 -0500

    init

---
+++ index.js
@@ -0,0 +1,73 @@
+const ServerMock = require("mock-http-server");
+const { mockWeatherResponses } = require("./mockweather");
+
+const port = process.env.PORT || 3030;
+const server = new ServerMock({
+  host: "localhost",
+  port,
+});
+
+const onStart = () => {
+  console.log(`Server listening on port ${port}`);
+};
+
+server.on({
+  method: "GET",
+  path: "/resource",
+  reply: {
+    status: 200,
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ hello: "world" }),
+  },
+});
+
+server.on({
+  method: "GET",
+  path: "/data/2.5/weather",
+  reply: {
+    status: () => {
+      return `${shouldFail ? 500 : 200}`;
+    },
+    headers: { "content-type": "application/json" },
+    body: () => {
+      return JSON.stringify(randWeather());
+    },
+  },
+});
+
+function shouldFail() {
+  return Math.random() > 0.5;
+}
+
+function randWeather() {
+  return {
+    weather: [
+      {
+        id: 804,
+        main: getRandResponse([
+          "Thunderstorm",
+          "Drizzle",
+          "Rain",
+          "Snow",
+          "Atmosphere",
+          "Clear",
+          "Mist",
+          "Snow",
+          "Clouds",
+        ]),
+        description: "overcast clouds",
+        icon: "04n",
+      },
+    ],
+    main: {
+      temp: Math.random() * 100,
+    },
+  };
+}
+
+function getRandResponse(responses) {
+  const index = Math.floor(Math.random() * responses.length);
+  return responses[index];
+}
+
+server.start(onStart);
---
+++ mockweather.js
@@ -0,0 +1,15 @@
+module.exports.mockWeatherResponses = [
+  {
+    weather: [
+      {
+        id: 804,
+        main: "Clouds",
+        description: "overcast clouds",
+        icon: "04n",
+      },
+    ],
+    main: {
+      temp: 74.3,
+    },
+  },
+];
